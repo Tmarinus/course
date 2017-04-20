3b200025f3cd77bb89c5c39321c0b4b82e42efb3
Rhythima <rhythimashinde@gmail.com>
2017-04-20 22:35:15 +0200

    k fold

--- FINAL/k_fold/k_fold.m
+++ FINAL/k_fold/k_fold.m
@@ -2,8 +2,8 @@
 d_size=50;
 true_hog=0;
 true_hog_average=0;
-true={};
-
+accuracy=zeros(1,10);
+d='svc'; %classifier
 r=randperm(1000); 
 rn=num2cell(reshape(r, 100, 10), 1);
 
@@ -21,7 +21,7 @@ for i=1:10
 
     % Training
     % build classifier for HOG feature
-     classifier_hog = classifier('svc',training_hog, training_labels);
+     classifier_hog = classifier(d,training_hog, training_labels);
     
     toc
 
@@ -30,8 +30,8 @@ for i=1:10
      predicted_labels_hog = predict(classifier_hog, test_hog);
      
     % calculate and/or display results
-     confusion_matrix_hog = confusionmat(test_labels, predicted_labels_hog_SVM);
-     helperDisplayConfusionMatrix(confusion_matrix_hog);
+     confusion_matrix_hog = confusionmat(test_labels, predicted_labels_hog);
+    % helperDisplayConfusionMatrix(confusion_matrix_hog);
      
      preditcted_strings_hog = cellstr(predicted_labels_hog);
     
@@ -43,8 +43,14 @@ for i=1:10
      e=true_hog/test_size*10;
      
      true_hog_average = true_hog_average + true_hog_prev;
-               
-     %get full results
-     disp(fprintf( 'Total %d, correct: %d, err: %d\n, accuracy: %d\n', test_size*10, true_hog, false_hog, e));
+     
+     %get the accuracy for each test run
+     accuracy(i)=true_hog/test_size;
+     
+     %print full results
+     %disp(fprintf( 'Total %d, correct: %d, err: %d\n, accuracy: %d\n', test_size*10, true_hog, false_hog, e));
      
 end
+
+%get the average accuracy of all runs
+%average_accuray= true_hog_average/test_size
